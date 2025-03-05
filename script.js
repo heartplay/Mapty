52643f5087f24404e47ef2d34775607f279b07a6
Gleb Fedoseev <heartplay.gf@gmail.com>
2025-03-06 00:43:20 +0500

    OOP refactoring

--- script.js
+++ script.js
@@ -11,65 +11,80 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// Leaflet map, leaflet event on map
-let map, mapEvent;
+class App {
+    #map;
+    #mapEvent;
+    constructor() {
+        this._getPosition();
+        form.addEventListener(`submit`, this._newWorkout.bind(this));
+        // Toggling cadence/elevation inputs after selecting running/cycling
+        inputType.addEventListener(`change`, this._toggleCadenceElevationInput);
+    }
 
-// browser geolocation API
-if (navigator.geolocation)
-    // Getting current coordinates from browser
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
-            const { latitude, longitude } = position.coords;
-            const coords = [latitude, longitude];
-            // Loading leaflet styled map according to coordinates
-            map = L.map('map', { attributionControl: false }).setView(coords, 13);
-            // Link to leaflet and openstreetmap
-            L.control.attribution().setPrefix('<a href="https://leafletjs.com/">Leaflet</a>').addTo(map);
-            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            }).addTo(map);
-            // Leaflet event listener for click on map
-            map.on(`click`, function (mapE) {
-                // Getting leaflet event object
-                mapEvent = mapE;
-                // Show form with inputs
-                form.classList.remove(`hidden`);
-                // Focus on distance input
-                inputDistance.focus();
-            });
-        },
-        // No coordinates
-        function () {
-            alert(`Could not get your position.`);
-        }
-    );
+    _getPosition() {
+        // Browser geolocation API
+        if (navigator.geolocation)
+            // Getting current coordinates from browser
+            navigator.geolocation.getCurrentPosition(
+                // If coordinates is recieved
+                this._loadMap.bind(this),
+                // No coordinates
+                () => alert(`Could not get your position.`)
+            );
+    }
 
-form.addEventListener(`submit`, function (e) {
-    // Prevent from reload page after submitting
-    e.preventDefault();
-    // Getting coordinates of click on map
-    const { lat, lng } = mapEvent.latlng;
-    // Putting marker on map according to coordinates
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-            L.popup({
-                maxWidth: 250, // max width of mark popup
-                minWidth: 100, // min width of mark popup
-                autoClose: false, // prevent closing popup when another popup is opened
-                closeOnClick: false, // prevent closing popup when click on map
-                className: `running-popup`, // assigning css class name to popup
-            })
-        )
-        .setPopupContent(`Workout`) // setting popup content
-        .openPopup();
-    // Clear all inputs after submitting
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ``;
-    inputDistance.focus();
-});
+    _loadMap(position) {
+        const { latitude, longitude } = position.coords;
+        // Loading leaflet styled map according to coordinates
+        this.#map = L.map('map', { attributionControl: false }).setView([latitude, longitude], 13);
+        // Link to leaflet and openstreetmap
+        L.control.attribution().setPrefix('<a href="https://leafletjs.com/">Leaflet</a>').addTo(this.#map);
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(this.#map);
+        // Leaflet event listener method for click on map
+        this.#map.on(`click`, this._showForm.bind(this));
+    }
 
-// Toggling cadence/elevation inputs after selecting running/cycling
-inputType.addEventListener(`change`, function () {
-    inputCadence.closest(`.form__row`).classList.toggle(`form__row--hidden`);
-    inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`);
-});
+    _showForm(mapE) {
+        // Getting leaflet event object
+        this.#mapEvent = mapE;
+        // Show form with inputs
+        form.classList.remove(`hidden`);
+        // Focus on distance input
+        inputDistance.focus();
+    }
+
+    _toggleCadenceElevationInput() {
+        // Toggling cadence/elevation inputs
+        inputCadence.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+        inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+    }
+
+    _newWorkout(e) {
+        // Prevent from reload page after submitting
+        e.preventDefault();
+        // Getting coordinates of click on map
+        const { lat, lng } = this.#mapEvent.latlng;
+        // Putting marker on map according to coordinates
+        L.marker([lat, lng])
+            .addTo(this.#map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250, // max width of mark popup
+                    minWidth: 100, // min width of mark popup
+                    autoClose: false, // prevent closing popup when another popup is opened
+                    closeOnClick: false, // prevent closing popup when click on map
+                    className: `running-popup`, // assigning css class name to popup
+                })
+            )
+            .setPopupContent(`Workout`) // setting popup content
+            .openPopup();
+        // Clear all inputs after submitting
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ``;
+        inputDistance.focus();
+    }
+}
+
+// Init app
+const app = new App();
